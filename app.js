6e850229514b57d1a6a57c646027ab5d80b16812
Andre <andre.eng1313@gmail.com>
2023-01-14 17:21:54 -0300

    Acertando a API de participantes

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ let db;
 
 mongoClient.connect()
 .then(() => {
-    db = mongoClient.db("api_uol")
+    db = mongoClient.db()
     console.log("foi daora mongodb!")
 })
 .catch(() => {
@@ -28,38 +28,33 @@ mongoClient.connect()
 
 
 
-app.post("/participants", (req, res) => {
+app.get("/participants", (req, res) => {
 
     const { name } = req.body
 
-   const parts = db.collection("participants").insertOne({ name, lastStatus: Date.now() })
-   .then(() => {
-    res.status(201).send("Deu certo!")
-   })
-   .catch(() => {
-    res.status(422).send("Um erro foi encontrado")
-   })
-
-   const parts2 = db.collection("participants").findOne({ name })
-
-   if (parts2)
-   return res.status(409).send("Este usuário já existe!")
-
+    const participants = db.collection("participants").find().toArray()
+    res.send(participants)
 
 })
 
 
+app.post("/participants", (req, res) => {
 
+    const { name } = req.body
 
+   const parts = db.collection("participants").insertOne({ name: name, lastStatus: Date.now() })
+   .then(() => {
+    res.status(201).send("Usuario cadastrado!")
+   })
+   .catch(() => {
+    res.status(422).send("Erro")
+   })
 
+   const parts2 = db.collection("participants").find({ name })
 
-
-
-
-
-
-
-
+   if (!parts2)
+   return res.status(409).send("Este usuário já existe!")
+})
 
 
 
